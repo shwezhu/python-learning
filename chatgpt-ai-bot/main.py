5a3379ea1de7d3df1f4be1692e414a8a5bbc4a1b
David Zhu <shaowenzhu@dal.ca>
2023-06-11 15:20:24 -0300

    feat: remember history

--- chatgpt-ai-bot/main.py
+++ chatgpt-ai-bot/main.py
@@ -1,5 +1,9 @@
 from gptbot import GPTBot
 
-
 bot = GPTBot()
-print(bot.makeChatRequest('Do you love cat?'))
+while True:
+    user_input = input("Enter your question (Type 'exit' to exit): ")
+    response = bot.makeChatRequest(user_input)
+    print(response['choices'][0]['message']['content'])
+    if user_input == 'exit':
+        break
