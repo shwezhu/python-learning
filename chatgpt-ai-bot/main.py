4a1a24e331ccb579f1dcb7f2c8cb1d2b3ef168a3
David Zhu <shaowenzhu@dal.ca>
2023-09-16 20:38:45 -0300

    refactor: remove gptbot

--- chatgpt-ai-bot/main.py
+++
@@ -1,9 +0,0 @@
-from gptbot import GPTBot
-
-bot = GPTBot()
-while True:
-    user_input = input("Enter your question (Type 'exit' to exit): ")
-    response = bot.makeChatRequest(user_input)
-    print(response['choices'][0]['message']['content'])
-    if user_input == 'exit':
-        break
